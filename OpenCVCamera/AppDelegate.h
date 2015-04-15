d545819ccd99a2ecb514b9abc5a0bf0a0e034daa
刘伟 <robbieliu2003@gmail.com>
2015-04-15 11:21:26 +0800

    Initial Commit

---
+++ OpenCVCamera/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OpenCVCamera
+//
+//  Created by 刘伟 on 15/4/15.
+//  Copyright (c) 2015年 刘伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
